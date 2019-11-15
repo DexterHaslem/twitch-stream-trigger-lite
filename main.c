74e4b2f931c05cf87c70dfd84d7843eec7b1efb2
Dexter Haslem <dexter@haslemtech.com>
2019-11-15 01:15:29 -0700

    messing with win32 controls

--- main.c
+++ main.c
@@ -4,14 +4,31 @@
 #define WINDOW_CLASSNAME ("ST2")
 #define WINDOW_WIDTH 500
 #define WINDOW_HEIGHT 300
+#define POLL_TIMER_ID (1)
+#define API_POLL_MS 10 * 1000
 
 #define ID_FILE_EXIT                    40001
 #define ID_ABOUT_LICENSES               40002
+
+
+static HWND hGroupBoxTrigger1;
+static HWND hStatus;
+
+// VOID(CALLBACK* TIMERPROC)(HWND, UINT, UINT_PTR, DWORD);
+void on_timer_expire(HWND hwnd, UINT msg, UINT_PTR timerId, DWORD dwTime)
+{
+	(void*)hwnd;
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
+	case WM_SIZE:
+		/* make groupboxes resize with parent - not really used, we disabled resize
+		MoveWindow(hGroupBoxTrigger1, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
+		*/
+		return 0;
 	case WM_CREATE:
 	{
 		HMENU hMenu, hSubMenu;
@@ -27,14 +44,45 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&About");
 
 		SetMenu(hwnd, hMenu);
+
+		hStatus = CreateWindowEx(0, STATUSCLASSNAME, NULL,
+			WS_CHILD | WS_VISIBLE, 0, 0, 0, 0,
+			hwnd, NULL, GetModuleHandle(NULL), NULL);
+		
+		hGroupBoxTrigger1 = CreateWindowEx(WS_EX_CLIENTEDGE, "BUTTON", "Trigger 1",
+			WS_CHILD | WS_VISIBLE | BS_GROUPBOX | WS_GROUP,
+			5, 5, 400, 150, hwnd, NULL, GetModuleHandle(NULL), NULL);
+				
+		HFONT hfDefault;
+		HWND hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "",
+			WS_CHILD | WS_VISIBLE,
+			5, 20, 200, 20, hGroupBoxTrigger1, NULL, GetModuleHandle(NULL), NULL);
+		if (hEdit == NULL)
+			MessageBox(hwnd, "Could not create edit box.", "Error", MB_OK | MB_ICONERROR);
+			hfDefault = GetStockObject(DEFAULT_GUI_FONT);
+		SendMessage(hEdit, WM_SETFONT, (WPARAM)hfDefault, MAKELPARAM(FALSE, 0));
+
+		/* start polling timer now */
+		SetTimer(hwnd, POLL_TIMER_ID, API_POLL_MS, on_timer_expire);
 		break;
 	}
 	case WM_CLOSE:
+		KillTimer(hwnd, POLL_TIMER_ID);
 		DestroyWindow(hwnd);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_EXIT:
+			PostMessage(hwnd, WM_CLOSE, 0, 0);
+			break;
+		case ID_ABOUT_LICENSES:
+			MessageBox(hwnd, "licenses", "About Licenses", MB_OK | MB_ICONERROR);
+			break;
+		}
 	default:
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
@@ -70,7 +118,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		WS_EX_CLIENTEDGE,
 		WINDOW_CLASSNAME,
 		WINDOW_TITLE,
-		WS_OVERLAPPEDWINDOW,
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
 		CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT,
 		NULL, NULL, hInstance, NULL);
 
@@ -89,5 +137,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
 	return msg.wParam;
 }
--- main.h
+++ main.h
@@ -2,10 +2,29 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <commctrl.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 #include "cJSON.h"
+
+
+struct stream_trigger
+{
+	bool enabled;
+	const char* account;
+	const char* cmd;
+	bool is_online;
+};
+
+struct all_triggers
+{
+	struct stream_trigger trigger_one;
+	struct stream_trigger trigger_two;
+	struct stream_trigger trigger_three;
+};
