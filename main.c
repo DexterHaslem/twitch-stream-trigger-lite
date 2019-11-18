e2e6d9b11b090ff108bc741df7a9906590c7994c
Dexter Haslem <dexter@haslemtech.com>
2019-11-18 04:33:06 -0700

    fix comm controls put on parent hwnd to capture messages
    create array dynamically along with generated ids to check

--- main.c
+++ main.c
@@ -1,63 +1,59 @@
 #include "main.h"
 
-#define WINDOW_TITLE ("StreamTrigger2")
+#define WINDOW_TITLE ("Twitch Stream Trigger v2")
 #define WINDOW_CLASSNAME ("ST2")
 #define WINDOW_WIDTH 380
-#define WINDOW_HEIGHT 430
+#define WINDOW_HEIGHT 550
 #define POLL_TIMER_ID (1)
 #define API_POLL_MS 1 * 1000
 
-#define ID_FILE_EXIT                    40001
-#define ID_ABOUT_LICENSES               40002
-#define ID_CHKBOX_TRIGGER1_ENABLE		40005
-#define ID_CHKBOX_TRIGGER2_ENABLE		40006
-#define ID_CHKBOX_TRIGGER3_ENABLE		40007
+/* this coudl be dynamic but just hardcode enough for now */
+#define NUM_HARDCODED_TRIGGERS 4
+
+#define ID_FILE_EXIT        40001
+#define ID_HELP_LICENSES    40002
+#define ID_HELP_ABOUT		40003
 
 static HWND hStatus;
-struct stream_trigger test = { 0 };
+struct stream_trigger triggers[NUM_HARDCODED_TRIGGERS];
 
-// VOID(CALLBACK* TIMERPROC)(HWND, UINT, UINT_PTR, DWORD);
 void on_timer_expire(HWND hwnd, UINT msg, UINT_PTR timerId, DWORD dwTime)
 {
-	/*
-	if (SendMessage(test.hEnabledCheckbox, BM_GETCHECK, 0, 0) == BST_UNCHECKED)
-	{
-
-	}
-	else
-	{
-
-	}*/
+	
 }
 
 void create_trigger_group(struct stream_trigger *trigger, int start_y, HWND owner)
 {
 	HINSTANCE hInstance = GetModuleHandle(NULL);// (HINSTANCE)GetWindowLong(owner, GWL_HINSTANCE);
 	
-	HWND hGroupBoxTrigger = CreateWindowEx(WS_EX_TRANSPARENT, WC_BUTTON, "Trigger",
+	char titlebuf[256];
+	sprintf_s(&titlebuf, 256, "Trigger %d", trigger->serialize.num);
+
+	HWND hGroupBoxTrigger = CreateWindowEx(WS_EX_TRANSPARENT, WC_BUTTON, titlebuf,
 		WS_CHILD | WS_VISIBLE | BS_GROUPBOX | WS_GROUP,
-		5, start_y, 320, 110, owner, NULL, hInstance, NULL);
+		5, start_y, 320, 100, owner, NULL, hInstance, NULL);
 	
+	/* TODO: set initial checked to state */
 	HWND hEnabled = CreateWindowEx(0, WC_BUTTON, "Enabled", WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX,
-		10, 15, 75, 25, hGroupBoxTrigger, (HMENU) trigger->enabledCheckboxId, hInstance, NULL);
+		10, start_y + 15, 75, 25, owner, (HMENU) trigger->enabledCheckboxId, hInstance, NULL);
 	trigger->hEnabledCheckbox = hEnabled;
 
 	HWND hAccountLabel = CreateWindowEx(0, WC_STATIC, "Account:", WS_CHILD | WS_VISIBLE, 
-		10, 45, 100, 25, hGroupBoxTrigger,
+		10, start_y + 45, 100, 25, owner,
 		NULL, hInstance, NULL);
 	
 	HWND hEditAccount = CreateWindowEx(WS_EX_CLIENTEDGE, WC_EDIT, "Account here",
 		WS_CHILD | WS_VISIBLE,
-		75, 40, 230, 25, hGroupBoxTrigger, NULL, hInstance, NULL);
+		75, start_y + 40, 230, 25, owner, NULL, hInstance, NULL);
 	trigger->hEditAccount = hEditAccount;
 
 	HWND hCmdLabel = CreateWindowEx(0, WC_STATIC, "Command:", WS_CHILD | WS_VISIBLE,
-		10, 75, 100, 25, hGroupBoxTrigger,
+		10, start_y + 75, 100, 25, owner,
 		NULL, hInstance, NULL);
 
 	HWND hEditCommandLine = CreateWindowEx(WS_EX_CLIENTEDGE, WC_EDIT, "Command line here",
 		WS_CHILD | WS_VISIBLE,
-		75, 70, 230, 25, hGroupBoxTrigger, NULL, hInstance, NULL);
+		75, start_y + 70, 230, 25, owner, NULL, hInstance, NULL);
 	trigger->hEditCommand = hEditCommandLine;
 
 	HFONT hfDefault = GetStockObject(DEFAULT_GUI_FONT);
@@ -69,23 +65,22 @@ void create_trigger_group(struct stream_trigger *trigger, int start_y, HWND owne
 	SendMessage(hEditAccount, WM_SETFONT, (WPARAM)hfDefault, MAKELPARAM(FALSE, 0));
 }
 
+static void setup_triggers()
+{
+	for (int i = 0; i < NUM_HARDCODED_TRIGGERS; ++i)
+	{
+		/* NOTE: do not overlap resource ids if anything else gets added */
+		triggers[i].enabledCheckboxId = 50000 + i;
+		triggers[i].serialize.num = i + 1;
+	}
+
+	/* TODO: restoration of account/cmd here */
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-	case WM_CTLCOLORSTATIC:
-	{
-		//HDC hdc = (HDC)wParam;
-		//HWND hwnd = (HWND)lParam;
-		//COLORREF c = 0x000000FF;
-		//SetBkColor(hdc, c); // Set to red
-		//SetDCBrushColor(hdc, c);
-
-		//DWORD CtrlID = GetDlgCtrlID((HWND)lParam);
-		//HDC hdcStatic = (HDC)wParam;
-		//SetBkMode(hdcStatic, TRANSPARENT);
-		break;
-	}
 	case WM_SIZE:
 		/* make groupboxes resize with parent - not really used, we disabled resize
 		MoveWindow(hGroupBoxTrigger1, 0, 0, LOWORD(lParam), HIWORD(lParam), TRUE);
@@ -102,8 +97,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&File");
 
 		hSubMenu = CreatePopupMenu();
-		AppendMenu(hSubMenu, MF_STRING, ID_ABOUT_LICENSES, "&Licenses");
-		AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&About");
+		AppendMenu(hSubMenu, MF_STRING, ID_HELP_LICENSES, "&Licenses");
+		AppendMenu(hSubMenu, MF_STRING, ID_HELP_ABOUT, "&About");
+		AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&Help");
 
 		SetMenu(hwnd, hMenu);
 
@@ -111,13 +107,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			WS_CHILD | WS_VISIBLE, 0, 0, 0, 0,
 			hwnd, NULL, GetModuleHandle(NULL), NULL);
 		
-		
-		test.enabledCheckboxId = ID_CHKBOX_TRIGGER1_ENABLE;
-		create_trigger_group(&test, 5, hwnd);
+		setup_triggers();
 
-		//create_trigger_group(NULL, 115, hwnd);
-
-		//create_trigger_group(NULL, 225, hwnd);
+		for (int i = 0; i < NUM_HARDCODED_TRIGGERS; ++i)
+		{
+			create_trigger_group(&triggers[i], (i * 110 + 5), hwnd);
+		}
 
 		/* start polling timer now */
 		SetTimer(hwnd, POLL_TIMER_ID, API_POLL_MS, on_timer_expire);
@@ -131,32 +126,39 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		break;
 	case WM_COMMAND:
-		switch (LOWORD(wParam))
+	{
+		WORD from = LOWORD(wParam);
+		switch (from)
 		{
 		case ID_FILE_EXIT:
 			PostMessage(hwnd, WM_CLOSE, 0, 0);
 			break;
-		case ID_ABOUT_LICENSES:
+		case ID_HELP_LICENSES:
 			MessageBox(hwnd, "licenses", "About Licenses", MB_OK | MB_ICONERROR);
 			break;
-		case ID_CHKBOX_TRIGGER1_ENABLE:
+		case ID_HELP_ABOUT:
+			/* TODO: */
+			break;
+		default:
 		{
-			switch (HIWORD(wParam))
+			/* check if it was one of our trigger enable checkboxes */
+			for (int i = 0; i < NUM_HARDCODED_TRIGGERS; ++i)
 			{
-			case BN_CLICKED:
-				if (SendDlgItemMessage(hwnd, ID_CHKBOX_TRIGGER1_ENABLE, BM_GETCHECK, 0, 0))
+				if (from == triggers[i].enabledCheckboxId)
 				{
-					//MessageBox(NULL, "Checkbox Selected", "Success", MB_OK | MB_ICONINFORMATION);
+					if (HIWORD(wParam) == BN_CLICKED)
+					{
+						/* have to ask current state everytime */
+						bool enabled = SendDlgItemMessage(hwnd, from, BM_GETCHECK, 0, 0);
+						triggers[i].serialize.enabled = enabled;
+					}
+					break;
 				}
-				else
-				{
-					//MessageBox(NULL, "Checkbox Unselected", "Success", MB_OK | MB_ICONINFORMATION);
-				}
-				break;
 			}
 		}
 		}
 		break;
+	}
 	default:
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
